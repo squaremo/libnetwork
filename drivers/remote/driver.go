2c77c87a2f6e77880c7801fd0a3527e0928a8df5
Michael Bridgen <mikeb@squaremobius.net>
2015-05-15 17:18:52 +0100

    WIP on implementing remote driver

--- drivers/remote/driver.go
+++ drivers/remote/driver.go
@@ -2,6 +2,7 @@ package remote
 
 import (
 	"errors"
+	"net"
 
 	"github.com/docker/libnetwork/driverapi"
 	"github.com/docker/libnetwork/sandbox"
@@ -33,34 +34,120 @@ func (d *driver) Config(option map[string]interface{}) error {
 	return driverapi.ErrNotImplemented
 }
 
+type createNetwork struct {
+	Id      string
+	Options map[string]interface{}
+}
+
 func (d *driver) CreateNetwork(id types.UUID, option map[string]interface{}) error {
-	return driverapi.ErrNotImplemented
+	return d.client.Call("NetworkDriver.CreateNetwork", &createNetwork{string(id), option}, nil)
 }
 
 func (d *driver) DeleteNetwork(nid types.UUID) error {
 	return driverapi.ErrNotImplemented
 }
 
+type createEndpoint struct {
+	NetworkId string
+	Id        string
+	Options   map[string]interface{}
+}
+
+type endpointInterface struct {
+	SrcName     string
+	DstName     string
+	Address     string
+	AddressIPv6 string
+}
+
+type createEndpointResponse struct {
+	Interfaces  []*endpointInterface
+	Gateway     string
+	GatewayIPv6 string
+}
+
+// TODO: IPv6, Gateway etc.
+func (r *createEndpointResponse) toSandboxInfo() (*sandbox.Info, error) {
+	var (
+		ifaces []*sandbox.Interface = make([]*sandbox.Interface, len(r.Interfaces))
+	)
+	for i, inIf := range r.Interfaces {
+		outIf := &sandbox.Interface{
+			SrcName: inIf.SrcName,
+			DstName: inIf.DstName,
+		}
+		ip, ipnet, err := net.ParseCIDR(inIf.Address)
+		if err != nil {
+			return nil, err
+		}
+		ipnet.IP = ip
+		outIf.Address = ipnet
+		ifaces[i] = outIf
+	}
+	return &sandbox.Info{
+		Interfaces:  ifaces,
+		Gateway:     nil,
+		GatewayIPv6: nil,
+	}, nil
+}
+
 func (d *driver) CreateEndpoint(nid, eid types.UUID, epOptions map[string]interface{}) (*sandbox.Info, error) {
-	return nil, driverapi.ErrNotImplemented
+	var res createEndpointResponse
+
+	create := &createEndpoint{
+		string(nid),
+		string(eid),
+		epOptions,
+	}
+
+	if err := d.client.Call("NetworkDriver.CreateEndpoint", create, &res); err != nil {
+		return nil, err
+	}
+	if info, err := res.toSandboxInfo(); err != nil {
+		return nil, err
+	} else {
+		return info, nil
+	}
+}
+
+type deleteEndpoint struct {
+	NetworkId  string
+	EndpointId string
 }
 
 func (d *driver) DeleteEndpoint(nid, eid types.UUID) error {
-	return driverapi.ErrNotImplemented
+	return d.client.Call("NetworkDriver.DeleteEndpoint", &deleteEndpoint{string(nid), string(eid)}, nil)
 }
 
 func (d *driver) EndpointInfo(nid, eid types.UUID) (map[string]interface{}, error) {
-	return nil, driverapi.ErrNotImplemented
+	return map[string]interface{}{}, nil
+}
+
+type join struct {
+	NetworkId  string
+	EndpointId string
+	SandboxKey string
+	Options    map[string]interface{}
 }
 
 // Join method is invoked when a Sandbox is attached to an endpoint.
 func (d *driver) Join(nid, eid types.UUID, sboxKey string, options map[string]interface{}) (*driverapi.JoinInfo, error) {
-	return nil, driverapi.ErrNotImplemented
+	var info driverapi.JoinInfo
+	if err := d.client.Call("NetworkDriver.Join", &join{string(nid), string(eid), sboxKey, options}, &info); err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
+
+type leave struct {
+	NetworkId  string
+	EndpointId string
+	Options    map[string]interface{}
 }
 
 // Leave method is invoked when a Sandbox detaches from an endpoint.
 func (d *driver) Leave(nid, eid types.UUID, options map[string]interface{}) error {
-	return driverapi.ErrNotImplemented
+	return d.client.Call("NetworkDriver.Leave", &leave{string(nid), string(eid), options}, nil)
 }
 
 func (d *driver) Type() string {
